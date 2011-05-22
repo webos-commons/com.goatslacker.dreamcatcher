14fada9a9ec30e056bfe4fea2f522c6f81b45e0d
Josh Perez <josh@goatslacker.com>
2011-05-22 13:46:58 -0700

    added a Jakefile build method

---
+++ Jakefile.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const sys = require("sys");
+const fs = require("fs");
+const exec = require('child_process').exec;
+
+/**
+  Returns a function to be executed
+  @param cmd {string} the command to run
+  @param callback {Function} the Function to execute after the command finishes running
+  */
+var run = function (cmd, callback) {
+  return function () {
+    console.log("Now running: " + cmd);
+    exec(cmd, function (err, stdout) {
+      if (err) {
+        throw err;
+      }
+
+      console.log(stdout);
+
+      if (callback) {
+        callback();
+      }
+    });
+  }
+},
+
+/**
+  Executes a set of instructions -- recursively builds using the run() function
+  @param instructions {Array} the commands to execute
+  @return Function
+  */
+build = function (instructions) {
+  var cmd = null;
+
+  if (instructions.length === 0) {
+    return cmd;
+  } else {
+    cmd = instructions.shift();
+    return run(cmd, build(instructions));
+  }
+};
+
+/** Tasks */
+
+task("install", [], function () {
+  var version = arguments[0] ? arguments[0].version : 0,
+
+  instructions = [
+    "palm-package application",
+    "palm-install com.goatslacker.dreamcatcher_" + version + "_all.ipk"
+  ],
+
+  go = build(instructions);
+
+  // run the listed commands
+  go();
+}, false);
